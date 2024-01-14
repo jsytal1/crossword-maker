c32a1caceb024e48bf3309f2e5e1686f4d07134c
jsytal1 <jsytal1@gmail.com>
2024-01-13 20:32:17 -0500

    setup react app

---
+++ packages/frontend/src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Crossword Maker</h1>
+        <p className="text-muted">A Crossword Building app</p>
+      </div>
+    </div>
+  );
+}
--- packages/functions/src/billing.ts
+++ packages/functions/src/billing.ts
@@ -6,7 +6,7 @@ import { calculateCost } from "@crossword-maker/core/cost";
 export const main = handler(async (event) => {
   const { storage, source } = JSON.parse(event.body || "{}");
   const amount = calculateCost(storage);
-  const description = "Scratch charge";
+  const description = "Crossword Maker charge";
 
   // Load our secret key
   const stripe = new Stripe(Config.STRIPE_SECRET_KEY, {
